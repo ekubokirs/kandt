0e9c9f496b710ad34e9b0ea74bc4154d667b13e2
Emilia <ekubokirs@gmail.com>
2014-06-25 21:32:32 -0700

    created comments model

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140626042259) do
+ActiveRecord::Schema.define(version: 20140626043131) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: true do |t|
+    t.string   "comment"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "communities", force: true do |t|
     t.string   "name"
