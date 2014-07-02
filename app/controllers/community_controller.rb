2b240e50483374b7dac86468ff406a629eaa2f9b
Emilia <ekubokirs@gmail.com>
2014-07-01 20:55:20 -0700

    started points and community controllers and error message handling

--- app/controllers/community_controller.rb
+++ app/controllers/community_controller.rb
@@ -1,14 +1,28 @@
 class CommunityController < ApplicationController
   def index
+    @communities = Community.all
   end
 
   def new
+    @community = Community.new
+    @errors = "shared/noErrors"
   end
 
   def create
+    @community = Community.new(community_params)
+    
+    @community.save
+    if @community.errors.any?
+      render :new
+      @errors = "errors"
+    else
+      redirect_to community_url
+    end
+
   end
 
   def edit
+    @community = Community.find_by(params[:id])
   end
 
   def update
@@ -17,4 +31,12 @@ class CommunityController < ApplicationController
   def destroy
   end
   
+  private
+
+  def community_params
+    params.require(:communit).permit(
+      :name,
+      :password
+    )
+  end
 end
--- app/controllers/point_controller.rb
+++ app/controllers/point_controller.rb
@@ -1,20 +1,27 @@
 class PointController < ApplicationController
 
   def index
+    @community = Community.find(params[:id])
+    @points = @community.points.all
   end
 
   def new
+    @point = Point.new
+    @rule   = community.rules
   end
 
   def create
   end
 
   def edit
+    @point = Point.find(params[:id])
   end
 
   def update
   end
 
   def destroy
+    point = Point.find(params[:id])
+    point.destroy
   end
 end
